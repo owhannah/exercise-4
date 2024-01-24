e4807cfb48788bc95323daa6c50527b270723461
Your Name <you@example.com>
2024-01-24 14:33:55 +0800

    remove_first_n_characters_from_a_string

--- remove_first_n_characters_from_a_string.py
+++ remove_first_n_characters_from_a_string.py
@@ -1,1 +1,14 @@
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# Example 
+# string:"pynative"
+# number to remove: 4
+
+def remove_first_n_chars(input_string, number):
+    return input_string[number:]
+
+user_input = input("Enter a string: ")
+number_to_remove = int(input("Enter the number of characters to remove: "))
+
+result_string = remove_first_n_chars(user_input, number_to_remove)
+print(f"New string: {result_string}")
